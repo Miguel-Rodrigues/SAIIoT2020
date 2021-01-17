9066de55b7197ebdf72d6a8786b7e0b68e1b4bcc
Miguel Rodrigues <miguelcrodrigues@outlook.com>
2021-01-17 20:40:52 +0000

    Update install.sh

--- Project/Src/accelKartServer/install.sh
+++ Project/Src/accelKartServer/install.sh
@@ -5,7 +5,7 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get autoremove
-apt-get install apache2-dev apache2-mpm-worker libapache2-mod-wsgi-py3 -y
+apt-get install apache2-dev apache2-mpm-worker libapache2-mod-wsgi-py3 python3 git -y
 
 # install python virtual environment
 pip3 install virtualenv
@@ -14,34 +14,19 @@ source ./env/bin/activate
 pip install -r ./requirements.txt
 deactivate
 
-# TODO: configure apache host
-# <VirtualHost *:80>
-#     ServerAdmin webmaster@localhost
-#     DocumentRoot /var/www/html
-#     ErrorLog ${APACHE_LOG_DIR}/error.log
-#     CustomLog ${APACHE_LOG_DIR}/access.log combined
-#     Alias /static ${rootPath}/static
-    
-#     <Directory ${rootPath}/static> 
-#         Require all granted
-#     </Directory>
-  
-#     <Directory ${rootPath}/${applicationName}>
-#         <Files wsgi.py>
-#             Require all granted
-#         </Files>
-#     </Directory>
-  
-#     WSGIDaemonProcess dvds python-path=${rootPath} python-home=${rootPath}/${env}
-#     WSGIProcessGroup dvds
-#     WSGIScriptAlias / #{rootPath}/${applicationName}/wsgi.py
-# </VirtualHost>
+#Virtualhost Manage Script
+wget -O virtualhost https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost.sh
+chmod +x virtualhost
+wget -O virtualhost-nginx https://raw.githubusercontent.com/RoverWire/virtualhost/master/virtualhost-nginx.sh
+chmod +x virtualhost-nginx
+
+virtualhost create "accelKartServer.local" cd ..
 
 # Configure permissions
-chmod g+w ${rootPath}/${applicationName}/db.sqlite3
-chmod g+w ${rootPath}/${applicationName}
+chmod g+w /var/www/accelkartServer/db.sqlite3
+chmod g+w /var/www/accelkartServer
 chown :www-data db.sqlite3
-chown :www-data ${rootPath}/${applicationName}
+chown :www-data /var/www/accelkartServer
 
 #restart server
 service apache2 restart
