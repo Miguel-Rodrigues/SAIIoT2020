e9b7d68b7ef8a89225a2130812dfd3ca97ce4b0f
Miguel Rodrigues <miguelcrodrigues@outlook.com>
2021-01-17 18:11:50 +0000

    Complete frontend

--- Project/Src/accelKartServer/WebProject/AccelKartServer/static/scripts/index.js
+++ Project/Src/accelKartServer/WebProject/AccelKartServer/static/scripts/index.js
@@ -1,8 +1,66 @@
 $(document).ready(function () {
-    var static = nipplejs.create({
+    var joypad = nipplejs.create({
         zone: $("#joypad")[0],
         mode: 'static',
         position: {left: '50%', top: '50%'},
-        color: 'red'
+        color: 'red',
+        
     });
+
+    joypad.on("move", function(evt, data) {
+        controls.angle = data.angle.radian;
+        controls.force = data.force;
+        MoveKart();
+    });
+
+    $("#button1").mousedown(function () {
+        controls.button1 = true;
+        MoveKart();
+
+    });
+
+    $("#button1").mouseup(function () {
+        controls.button1 = false;
+        MoveKart();
+    });
+
+    $("#button2").mousedown(function () {
+        controls.button2 = true;
+        MoveKart();
+    });
+
+    $("#button2").mouseup(function () {
+        controls.button2 = false;
+        MoveKart();
+    });
+    
+    var refresh = true;
+    var controls = {
+        angle: 0,
+        force: 0,
+        button1: false,
+        button2: false
+    };
+
+    function MoveKart() {
+        if (refresh) {
+            refresh = false;
+            setTimeout(function() {
+                refresh = true;
+            }, 200);
+    
+            $.post("/AccelKartServer", controls)
+                .done(function(data, textStatus) {
+                    $("#output").val(
+                        $("#output").val() +
+                        "[" + new Date().toISOString() + "][" + textStatus + "] Got response: " + JSON.stringify(data) + "\n");
+                })
+                .fail(function(xhr, textStatus) {
+    
+                    $("#output").val(
+                        $("#output").val() + "\n" +
+                        "[" + new Date().toISOString() + "][" + textStatus + "] Failed to get response: " + xhr.responseText + "\n");
+                });
+        }
+    }
 });
