e445659157fef08131cfa079de25ceb308cf1e38
Miguel Rodrigues <miguelcrodrigues@outlook.com>
2021-01-21 17:16:22 +0000

    disable staticFiles_DIRS

--- Project/Src/AccelKartServer/WebProject/web_project/settings.py
+++ Project/Src/AccelKartServer/WebProject/web_project/settings.py
@@ -128,4 +128,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
