968edcc5c2bdf52a5fef9871f0e9f29630fa5c16
Miguel Rodrigues <miguelcrodrigues@outlook.com>
2021-01-25 13:05:04 +0000

    update driver service

--- Project/Src/AccelKartServer/WebProject/AccelKartServer/services/KartDriverService.py
+++ Project/Src/AccelKartServer/WebProject/AccelKartServer/services/KartDriverService.py
@@ -9,41 +9,7 @@ import logging
 import math as math
 import copy as copy
 
-class SingletonMeta(type):
-    """
-    This is a thread-safe implementation of Singleton.
-    """
-
-    _instances = {}
-
-    _lock: Lock = Lock()
-    """
-    We now have a lock object that will be used to synchronize threads during
-    first access to the Singleton.
-    """
-
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
-        with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
-            if cls not in cls._instances:
-                instance = super().__call__()
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-class KartDriverService(metaclass=SingletonMeta):
+class KartDriverService():
     __watchdog: WatchdogService
     __leftMotorPin1 = 26
     __leftMotorPin2 = 19
@@ -65,7 +31,6 @@ class KartDriverService(metaclass=SingletonMeta):
     # __rightPWM2: GPIO.PWM
 
     __logger: logging.Logger
-    __calibration: SensorData = None
 
     def __init__(self):
         self.__logger = logging.getLogger(__name__)
