a9c9b8cdb0e90c2caaaeec235198c3bfffe4e8a4
Miguel Rodrigues <miguelcrodrigues@outlook.com>
2021-02-04 02:46:25 +0000

    add file logging

--- Project/Src/AccelKartServer/WebProject/web_project/settings.py
+++ Project/Src/AccelKartServer/WebProject/web_project/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGGING = {
     'version': 1,
@@ -145,9 +145,15 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
         },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/var/log/AccelKartServer.log'
+        }
     },
     'root': {
-        'handlers': ['console'],
+        'handlers': ['console', 'file'],
         'level': 'DEBUG',
     },
 }
